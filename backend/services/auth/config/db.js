032235d4c50f9955be997701f82a00b0c1c6c75c
Aniket Lotlikar <alotlikar98@gmail.com>
2026-07-24 00:04:22 +0530

    feat: add MongoDB connection utility with custom DNS configuration

--- backend/services/auth/config/db.js
+++ backend/services/auth/config/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import dns from 'node:dns';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 const connectDB = async () => {
     const url = process.env.MONGO_URI;
     if (!url) {
