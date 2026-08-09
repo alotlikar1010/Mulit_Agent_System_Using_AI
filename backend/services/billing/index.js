7ae4795061a65f4ec9a2ca63355bf59f9e430389
Aniket Lotlikar <alotlikar98@gmail.com>
2026-08-09 15:55:54 +0530

    billing services

---
+++ backend/services/billing/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+// import router from "./routes/auth.route.js"
+import connectDB from "./config/db.js"
+// import morgan from "morgan";
+// import mongoose from "mongoose";
+// import cors from "cors";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+const app = express();
+app.use(express.json());
+// app.use("/", router);
+
+app.listen(PORT, () => {
+    console.log(`Billing started at Port ${PORT}`);
+});
